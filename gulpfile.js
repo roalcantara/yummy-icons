782263f994a86a189a6545502d8281e6c3ad82d7
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2018-06-10 18:57:09 -0300

    build(tasks): Add Sass tasks
    Which compiles, renames, minifies and copies sass files
    to the dist folder.
    
    More:
    - https://github.com/hparra/gulp-rename
    -  https://www.npmjs.com/package/gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,36 @@
 
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 gulp.task('dist:clean', function () {
   return gulp.src('./dist', {read: false})
     .pipe(clean());
 });
 
+gulp.task('sass:compile', function () {
+  return gulp.src('./src/sass/yummy-icons.sass')
+    .pipe(sass({
+      outputStyle: 'extended'
+    })
+    .on('error', sass.logError))
+    .pipe(concat('yummy-icons.css'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass:minify', function () {
+  return gulp.src('./src/sass/yummy-icons.sass')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+    .on('error', sass.logError))
+    .pipe(concat('yummy-icons.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('build', [
-  'dist:clean'
+  'dist:clean',
+  'sass:compile',
+  'sass:minify'
 ]);
